d333b4a152cd40a660eebd45540d242fcd5887a0
JPB <bicepjpb@yahoo.fr>
2020-12-11 12:27:07 +0100

    démarrage du projet

---
+++ js/app.js
@@ -0,0 +1,33 @@
+/*
+<div class="todo">
+  <h3>Faire la vaisselle</h3>
+  <button>X</button>
+</div> 
+*/
+
+function createTodo(todoLabel) {
+  // declaration
+  const divTodo = document.createElement('div');  
+  const h3Todo = document.createElement('h3');
+  const btTodo = document.createElement('button');
+  const txtTodo = document.createTextNode(todoLabel);
+  const txtBtTodo = document.createTextNode("X");
+  const divTodos = document.querySelector('.todos');
+  
+  // configuration
+  divTodo.className = 'todo';
+  
+  // imbrication
+  h3Todo.appendChild(txtTodo);
+  divTodo.appendChild(h3Todo);
+  divTodo.appendChild(btTodo);
+  btTodo.appendChild(txtBtTodo);
+  
+  // insertion dans le DOM
+  divTodos.appendChild(divTodo);
+}
+
+document.formAddTodo.onsubmit = function(e) {
+  e.preventDefault();
+  createTodo(document.formAddTodo.todoTxt.value);
+}

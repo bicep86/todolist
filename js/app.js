737a0cb6465330559b37d2f5dfb551aeebba4c9b
JPB <bicepjpb@yahoo.fr>
2020-12-15 14:02:15 +0100

    commentaire explicatif du code

--- js/app.js
+++ js/app.js
@@ -6,25 +6,37 @@
 */
 
 function createTodo(todoLabel) {
-  // declaration
-  const divTodo = document.createElement('div');  
+  // DECLARATION
+  const divTodo = document.createElement('div'); 
+  // <div></div> 
   const h3Todo = document.createElement('h3');
+  // <h3></h3>
   const btTodo = document.createElement('button');
+  // <button></button>
   const txtTodo = document.createTextNode(todoLabel);
+  // Faire la vaisselle (exemple)
   const txtBtTodo = document.createTextNode("X");
+  // X
   const divTodos = document.querySelector('.todos');
+  // <div class="todos"></div> (existe déjà dans ton code html)
   
-  // configuration
+  // CONFIGURATION
   divTodo.className = 'todo';
+  // <div class="todo"></div> 
   
-  // imbrication
+  // IMBRICATION
   h3Todo.appendChild(txtTodo);
+  // <h3>Faire la vaisselle (exemple)</h3>
   divTodo.appendChild(h3Todo);
+  // <div class="todo"><h3>Faire la vaisselle (exemple)</h3></div> 
   divTodo.appendChild(btTodo);
+  // <div class="todo"><h3>Faire la vaisselle (exemple)</h3><button>X</button></div> 
   btTodo.appendChild(txtBtTodo);
+  // <button>X</button>
   
-  // insertion dans le DOM
+  // INSERTION DANS LE DOM
   divTodos.appendChild(divTodo);
+  // <div class="todos"><div class="todo"><h3>Faire la vaisselle (exemple)</h3><button>X</button></div></div>
 }
 
 document.formAddTodo.onsubmit = function(e) {
